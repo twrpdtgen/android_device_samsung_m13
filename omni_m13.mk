0e7e3f1569c46a30ae8b999fb2ee7812427e1a7c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-10-26 12:01:46 +0200

    m13: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: samsung Model: SM-M135FU Script version: 3.0.0

---
+++ omni_m13.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from m13 device
+$(call inherit-product, device/samsung/m13/device.mk)
+
+PRODUCT_DEVICE := m13
+PRODUCT_NAME := omni_m13
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-M135FU
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="m13ins-user 13 TP1A.220624.014 M135FUXXS4CWH1 release-keys"
+
+BUILD_FINGERPRINT := samsung/m13ins/m13:13/TP1A.220624.014/M135FUXXS4CWH1:user/release-keys
